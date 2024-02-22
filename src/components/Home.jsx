429d828f7b739d6ee1223ff8ae0335095e805786
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-02-21 17:20:43 -0700

    add home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+
+function Home(){
+    return (
+        <div>
+            <h1>Doomscroll</h1>
+            <p>A politics-free, fun, and comforting feed of the end times (right now).</p>
+        </div>
+    )
+}
