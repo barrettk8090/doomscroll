6583847dee9415c0b9206cee1fa011f12fa6c095
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-02-28 15:00:43 -0700

    api call feed

--- src/components/FeedMain.jsx
+++ src/components/FeedMain.jsx
@@ -1,12 +1,22 @@
 import { useState, useEffect } from "react";
 
-
 function FeedMain(){
+
+    const [feed, setFeed] = useState([]);
+
+    useEffect(() => {
+        fetch(`/api/feeds`)
+            .then(r => r.json())
+            .then(data => setFeed(data))
+    }, []);
+
+    const feedName = feed.map((feed) => { return feed.name });
 
 
     return (
         <div>
             <h2>Feed Main</h2>
+            <p>{feedName}</p>
         </div>
     )
 }
