c4ccf355eea0cac7efc5f2fb7d7a5548b1f25dcb
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-03-07 12:59:48 -0700

    scraping

--- server/scraping.py
+++ server/scraping.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.nytimes.com/2024/01/30/science/antarctica-bird-flu-penguins.html"
+nytimes_climate = "https://rss.nytimes.com/services/xml/rss/nyt/Climate.xml"
 
 proxy_addresses = {
     'http': 'http://72.206.181.123:4145',
     'https': 'http://191.96.100.33:3128'
 }
 
-result = requests.get(url, proxies=proxy_addresses)
+result = requests.get(nytimes_climate)
 print(result)
