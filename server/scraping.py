71ca817a30fc6a2385a219e7f34158be73384e77
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-03-12 17:11:09 -0600

    more supabase troubleshooting smh

--- server/scraping.py
+++ server/scraping.py
@@ -96,6 +96,7 @@ entry = earthquakes_gov_doc.find_all("entry")
 def earthquakes_gov_to_supabase():
     for earthquake in entry:
         title_text = earthquake.find('title').string
+        # use reg ex to separate out magnitude and location
         pattern = r'M\s*(\d+\.\d+)\s*-\s*(.+)'
         match = re.search(pattern, title_text)
 
@@ -103,8 +104,11 @@ def earthquakes_gov_to_supabase():
             # Extract magnitude and location
             earthquake_magnitude = match.group(1)
             earthquake_location = match.group(2).strip()
-
-            earthquake_depth = (earthquake.find('dd')).string
+            # depth is located inside the SECOND <dd> tag
+            dd_tags = earthquake.find_all('dd')
+            if len(dd_tags) > 1:
+                earthquake_depth = dd_tags[1].string
+            # find link tag then pull from href for url
             link = earthquake.find('link')
             earthquake_url = (link['href'])
             data = {
