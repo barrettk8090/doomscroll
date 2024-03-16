faa49198fd755cfc55b757458c9817ff516d3fc2
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-03-15 19:45:06 -0600

    add AI reddit data

--- server/Reddit.py
+++ server/Reddit.py
@@ -35,10 +35,8 @@ def reddit_H5N1_to_supabase():
 ############################# C L I M A T E  N E W S ############################
             
 collapse_subreddit_climate = reddit.subreddit("collapse")
-
 #get flair templates
 flair_templates = collapse_subreddit_climate.flair.link_templates
-
 # Separate posts by the "Climate" flair
 flair_text_climate = "Climate"
 
@@ -75,7 +73,7 @@ def reddit_covid_to_supabase():
                 data = {
                     'title': covid_title,
                     'url': covid_url,
-                    'source': "Reddit",
+                    'source': "Reddit", 
                     'category_id': 3
                 }
                 response, error = supabase.table('news_item').insert(data).execute()
@@ -83,4 +81,44 @@ def reddit_covid_to_supabase():
                     print(f"Error inserting article: {error}")
                 else:
                     print(f"Inserted article: {covid_title}")
-reddit_covid_to_supabase()
+# reddit_covid_to_supabase()
+
+############################# C A T E G O R Y  4 ############################
+############################# A R T I F I C I A L  I N T E L L I G E N C E  N E W S ############################
+
+artificial_subreddit = reddit.subreddit("artificial")
+flair_text_ai_news = "News"
+
+def reddit_ai_to_supabase():
+    for post in artificial_subreddit.new(limit=100):
+        if post.link_flair_text == flair_text_ai_news:
+            ai_title = post.title.replace("'", "").replace("[", "").replace("]", "").replace("(", "").replace(")", "")
+            ai_url = post.url
+            data = {
+                'title': ai_title,
+                'url': ai_url,
+                'source': "Reddit",
+                'category_id': 4 
+            }
+            response, error = supabase.table('news_item').insert(data).execute()
+            if error:
+                    print(f"Error inserting article: {error}")
+            else:
+                    print(f"Inserted article: {ai_title}")
+reddit_ai_to_supabase()
+
+
+############################# C A T E G O R Y  5 ############################
+############################# U F O  N E W S ############################
+
+############################# C A T E G O R Y  6 ############################
+############################# S O L A R  A C T I V I T Y  N E W S ############################
+
+############################# C A T E G O R Y  7 ############################
+############################# H U R R I C A N E  N E W S ############################
+
+############################# C A T E G O R Y  8 ############################
+############################# E A R T H Q U A K E  N E W S ############################
+
+############################# C A T E G O R Y  9 ############################
+############################# C Y B E R  A T T A C K  N E W S ############################
